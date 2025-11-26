78056ffd93ec95961aa75362f533226e719d43e5
Pinho <rafaelpinhotsilva@gmail.com>
2025-11-26 12:17:47 +0000

    Added Player Viewer

---
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/view/elements/ElementViewer.java
@@ -0,0 +1,8 @@
+package com.ldtsfeup2526.bobTheDestructor.view.elements;
+
+import com.ldtsfeup2526.bobTheDestructor.gui.GUI;
+import com.ldtsfeup2526.bobTheDestructor.model.Position;
+
+public interface ElementViewer {
+    void draw(Position position, GUI gui);
+}
---
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/view/elements/PlayerViewer.java
@@ -0,0 +1,11 @@
+package com.ldtsfeup2526.bobTheDestructor.view.elements;
+
+import com.ldtsfeup2526.bobTheDestructor.gui.GUI;
+import com.ldtsfeup2526.bobTheDestructor.model.Position;
+
+public class PlayerViewer implements ElementViewer{
+
+    public void draw(Position position, GUI gui) {
+
+    }
+}
