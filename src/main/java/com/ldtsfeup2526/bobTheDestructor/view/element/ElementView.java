ce9227a9527f986fb2c21ebfe789f55a4fb17bcf
T0m2sT <pedrotomasteixeira@gmail.com>
2025-11-27 17:08:52 +0000

    Updated View

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/view/element/ElementView.java
+++
@@ -1,8 +0,0 @@
-package com.ldtsfeup2526.bobTheDestructor.view.element;
-
-import com.ldtsfeup2526.bobTheDestructor.gui.GUI;
-import com.ldtsfeup2526.bobTheDestructor.model.elements.ElementModel;
-
-public interface ElementView<T extends ElementModel> {
-    void draw(T element, GUI gui);
-}
