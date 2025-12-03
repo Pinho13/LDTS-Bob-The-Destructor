d74203ba5a59c913551890cc6e8a1e6cd99230eb
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-03 02:50:19 +0000

    Implemented Menu class

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/model/menu/Menu.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/menu/Menu.java
@@ -1,4 +1,36 @@
 package com.ldtsfeup2526.bobTheDestructor.model.menu;
 
+import java.util.List;
+
 public abstract class Menu {
+    private final List<Button> buttons;
+    private int currentButton = 0;
+
+    public Menu() {
+        this.buttons = createButtons();
+    }
+
+    protected abstract List<Button> createButtons();
+
+    public List<Button> getButtons() {
+        return buttons;
+    }
+
+    public int getNumberOfButtons() {
+        return buttons.size();
+    }
+
+    public void moveDown() {
+        currentButton++;
+        currentButton %= getNumberOfButtons();
+    }
+
+    public void moveUp() {
+        currentButton += getNumberOfButtons() - 1;
+        currentButton %= getNumberOfButtons();
+    }
+
+    public Button getCurrentButton() {
+        return buttons.get(currentButton);
+    }
 }
