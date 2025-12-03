a20fc76f7473dca591233f2439932c47cfbfbb8a
alexis-ramoss <up202404977@edu.fe.up.pt>
2025-12-03 12:07:53 +0000

    Updated state class to receive inputs

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/controller/Controller.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/controller/Controller.java
@@ -1,4 +1,10 @@
 package com.ldtsfeup2526.bobTheDestructor.controller;
 
+import com.ldtsfeup2526.bobTheDestructor.controller.input.Action;
+
+import java.util.List;
+
 public abstract class Controller<T> {
+    public void update(List<Action> actions) {
+    }
 }
--- src/main/java/com/ldtsfeup2526/bobTheDestructor/states/State.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/states/State.java
@@ -1,22 +1,27 @@
 package com.ldtsfeup2526.bobTheDestructor.states;
 
 import com.ldtsfeup2526.bobTheDestructor.controller.Controller;
+import com.ldtsfeup2526.bobTheDestructor.controller.input.Action;
+import com.ldtsfeup2526.bobTheDestructor.controller.input.ActionParser;
 import com.ldtsfeup2526.bobTheDestructor.gui.GUI;
 import com.ldtsfeup2526.bobTheDestructor.view.SpriteLoader;
 import com.ldtsfeup2526.bobTheDestructor.view.ViewerProvider;
 import com.ldtsfeup2526.bobTheDestructor.view.screens.ScreenViewer;
 
 import java.io.IOException;
+import java.util.List;
 
 public abstract class State<T> {
     private final T model;
     private final Controller<T> controller;
     private final ScreenViewer<T> screenViewer;
+    private ActionParser actionParser;
 
     public State(T model, SpriteLoader spriteLoader) throws IOException {
         this.model = model;
         this.screenViewer = createScreenViewer(new ViewerProvider(spriteLoader));
         this.controller = createController();
+        actionParser = new ActionParser();
     }
 
     public abstract ScreenViewer<T> createScreenViewer(ViewerProvider viewerProvider);
@@ -27,6 +32,7 @@ public abstract class State<T> {
     }
 
     public void update(GUI gui) throws IOException {
+        controller.update(actionParser.get());
         screenViewer.draw(gui);
     }
 }
