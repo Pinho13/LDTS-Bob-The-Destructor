013977205068bb4428c1a348d875fc13a843c1b6
alexis-ramoss <up202404977@edu.fe.up.pt>
2025-12-03 10:00:15 +0000

    Updated Scene class to intialize the elements

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/Scene.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/Scene.java
@@ -1,5 +1,6 @@
 package com.ldtsfeup2526.bobTheDestructor.model.game;
 
+import com.ldtsfeup2526.bobTheDestructor.model.Position;
 import com.ldtsfeup2526.bobTheDestructor.model.game.elements.BlockModel;
 import com.ldtsfeup2526.bobTheDestructor.model.game.elements.PlayerModel;
 
@@ -7,7 +8,10 @@ public class Scene {
     private PlayerModel player;
     private BlockModel[][] blocks;
 
-    public Scene(PlayerModel player) { this.player = player; }
+    public Scene() {
+        player = new PlayerModel(new Position(50, 50));
+        blocks = new BlockModel[10][10];
+    }
     public PlayerModel getPlayer() { return player; }
 
     public BlockModel[][] getBlocks() { return blocks; }
