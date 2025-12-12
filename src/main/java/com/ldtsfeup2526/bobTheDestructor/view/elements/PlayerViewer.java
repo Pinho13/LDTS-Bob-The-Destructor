f6584c9ce7d8e7eb7c87cb0e87218b5998e1c924
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-12 11:16:06 +0000

    Added Sprite Paths to PlayerViewer

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/view/elements/PlayerViewer.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/view/elements/PlayerViewer.java
@@ -1,7 +1,7 @@
 package com.ldtsfeup2526.bobTheDestructor.view.elements;
 
 import com.ldtsfeup2526.bobTheDestructor.gui.GUI;
-import com.ldtsfeup2526.bobTheDestructor.model.game.elements.Player.PlayerModel;
+import com.ldtsfeup2526.bobTheDestructor.model.game.elements.Player.*;
 import com.ldtsfeup2526.bobTheDestructor.view.ElementViewer;
 import com.ldtsfeup2526.bobTheDestructor.view.Sprite;
 import com.ldtsfeup2526.bobTheDestructor.view.SpriteLoader;
@@ -11,14 +11,34 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class PlayerViewer implements ElementViewer<PlayerModel> {
-    private final Map<String, Sprite> spriteMap;
+    private final Map<Class<?>, Sprite[]> spriteMap;
 
     public PlayerViewer(SpriteLoader spriteLoader) throws IOException {
         spriteMap = new HashMap<>();
-        spriteMap.put("sprites/player/player1.png", spriteLoader.get("sprites/player/player1.png"));
+
+        spriteMap.put(IdleState.class, new Sprite[] {
+            spriteLoader.get("sprites/player/player1.png")
+        });
+
+        spriteMap.put(WalkingState.class, new Sprite[] {
+                spriteLoader.get("sprites/player/player_walk1.png"),
+                spriteLoader.get("sprites/player/player_walk2.png"),
+                spriteLoader.get("sprites/player/player_walk3.png"),
+                spriteLoader.get("sprites/player/player_walk4.png")
+        });
+
+        spriteMap.put(JumpingState.class, new Sprite[] {
+                spriteLoader.get("sprites/player/player_jump1.png"),
+                spriteLoader.get("sprites/player/player_jump2.png")
+        });
+
+        spriteMap.put(FallingState.class, new Sprite[] {
+                spriteLoader.get("sprites/player/player_fall1.png"),
+                spriteLoader.get("sprites/player/player_fall2.png")
+        });
     }
     public void draw(PlayerModel model, GUI gui) {
-        Sprite sprite = spriteMap.get("sprites/player/player1.png");
+        Sprite sprite = spriteMap.get(IdleState.class)[0];
         sprite.draw(model.getPosition(), gui);
     }
 }
