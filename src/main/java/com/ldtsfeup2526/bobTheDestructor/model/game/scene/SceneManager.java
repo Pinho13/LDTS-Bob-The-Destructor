74af8e4ce312127f3db5db0120d894ca51f9d977
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-15 04:10:15 +0000

    Added Random Cave Path Getter

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/scene/SceneManager.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/scene/SceneManager.java
@@ -1,10 +1,18 @@
 package com.ldtsfeup2526.bobTheDestructor.model.game.scene;
 
+import java.io.File;
 import java.io.IOException;
+import java.net.URL;
+import java.util.Objects;
+import java.util.Random;
+
+import static com.sun.tools.attach.VirtualMachine.list;
 
 public class SceneManager {
     private final SceneBuilder sceneBuilder;
     private Scene scene;
+    private int numberOfCaves = 2;
+    private final Random random = new Random();
     public SceneManager (SceneBuilder sceneBuilder) throws IOException {
         this.sceneBuilder = sceneBuilder;
         this.scene = sceneBuilder.createScene();
@@ -16,5 +24,10 @@ public class SceneManager {
 
     public SceneBuilder getSceneBuilder() {
         return sceneBuilder;
+    }
+
+    public String getNextCavePath() {
+        int number = random.nextInt(1, numberOfCaves);
+        return "caves/cave" + String.valueOf(number);
     }
 }
