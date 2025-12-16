a1aef47736d2dc7fd6b5325108abb4c7932b3c8c
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-16 01:48:54 +0000

    Speed Reset When mining

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/elements/Player/MiningState.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/model/game/elements/Player/MiningState.java
@@ -1,11 +1,12 @@
 package com.ldtsfeup2526.bobTheDestructor.model.game.elements.Player;
 
-import com.ldtsfeup2526.bobTheDestructor.model.game.physics.Collider;
-import com.ldtsfeup2526.bobTheDestructor.model.spatials.Position;
+import com.ldtsfeup2526.bobTheDestructor.model.spatials.Vector;
 
 public class MiningState extends PlayerState{
     public MiningState(PlayerModel playerModel) {
         super(playerModel);
+        playerModel.getRigidBody().setAcceleration(new Vector(0, playerModel.getRigidBody().getAcceleration().getY()));
+        playerModel.getRigidBody().setVelocity(new Vector(0, playerModel.getRigidBody().getVelocity().getY()));
     }
 
     @Override
