e8bf2062feea2729a6bc28099a21f85721bba906
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-26 22:40:24 +0000

    Added GameSoundManager

---
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/sounds/GameSoundManager.java
@@ -0,0 +1,85 @@
+package com.ldtsfeup2526.bobTheDestructor.sounds;
+
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+import java.util.Objects;
+
+public class GameSoundManager extends SoundManager{
+    private String currentMusicPath;
+    private Clip currentMusicClip;
+
+    public GameSoundManager(SoundLoader soundLoader) {
+        super(soundLoader);
+    }
+
+
+    @Override
+    public void playMusic(String soundFilePath) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        if (Objects.equals(currentMusicPath, soundFilePath)) {
+            return;
+        }
+
+        if (currentMusicClip != null) {
+            currentMusicClip.stop();
+        }
+
+        currentMusicClip = soundLoader.get(soundFilePath);
+        currentMusicClip.loop(Clip.LOOP_CONTINUOUSLY);
+
+        applyVolumeToClip(currentMusicClip, musicVolume);
+
+        currentMusicClip.start();
+
+    }
+
+    @Override
+    public void stopMusic() {
+        if (currentMusicClip != null) {
+            currentMusicClip.stop();
+            currentMusicClip.close();
+            currentMusicClip = null;
+        }
+    }
+
+    @Override
+    public void playSFX(String soundFilePath) throws UnsupportedAudioFileException, LineUnavailableException, IOException {
+        Clip sfx = soundLoader.get(soundFilePath);
+
+        applyVolumeToClip(sfx, sfxVolume);
+
+        sfx.start();
+    }
+
+
+    @Override
+    public void setMasterVolume(float volume) {
+        this.masterVolume = volume;
+    }
+
+    @Override
+    public void setMusicVolume(float volume) {
+        this.musicVolume = volume;
+    }
+
+    @Override
+    public void setSFXVolume(float volume) {
+        this.sfxVolume = volume;
+    }
+
+    @Override
+    public float getMasterVolume() {
+        return masterVolume;
+    }
+
+    @Override
+    public float getMusicVolume() {
+        return musicVolume;
+    }
+
+    @Override
+    public float getSFXVolume() {
+        return sfxVolume;
+    }
+}
