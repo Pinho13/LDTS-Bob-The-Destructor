ab8f6a64aaac2627304785fca33ed633e9765af7
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-26 22:40:16 +0000

    Finished Sound Manager

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/sounds/SoundManager.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/sounds/SoundManager.java
@@ -1,15 +1,48 @@
 package com.ldtsfeup2526.bobTheDestructor.sounds;
 
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
+
 public abstract class SoundManager implements VolumeControl{
     protected final SoundLoader soundLoader;
+    protected float masterVolume = 1;
+    protected float musicVolume = 1;
+    protected float sfxVolume = 1;
 
     public SoundManager(SoundLoader soundLoader) {
         this.soundLoader = soundLoader;
     }
 
-    public abstract void playMusic(String soundFilePath);
+    protected float linearToDb(float volume) {
+        if (volume <= 0f){
+            return -80f;
+        }
+        return (float) (20.0 * Math.log10(volume));
+    }
+
+    protected float dbToLinear(float db) {
+        return (float) Math.pow(10.0, db / 20.0);
+    }
+
+    protected void applyVolumeToClip(Clip clip, float volume) {
+        if (clip == null) return;
+
+        if (!clip.isControlSupported(FloatControl.Type.MASTER_GAIN)) return;
+
+        float finalVolume = masterVolume * volume;
+
+        FloatControl control = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+
+        control.setValue(linearToDb(finalVolume));
+    }
+
+
+    public abstract void playMusic(String soundFilePath) throws UnsupportedAudioFileException, LineUnavailableException, IOException;
     public abstract void stopMusic();
 
-    public abstract void playSFXR(String soundFilePath);
+    public abstract void playSFX(String soundFilePath) throws UnsupportedAudioFileException, LineUnavailableException, IOException;
 
 }
