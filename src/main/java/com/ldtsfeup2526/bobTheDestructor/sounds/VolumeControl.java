dd11b285ff3473ca68c99056765ce208408aa539
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-26 18:11:45 +0000

    Changed volume type

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/sounds/VolumeControl.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/sounds/VolumeControl.java
@@ -1,6 +1,6 @@
 package com.ldtsfeup2526.bobTheDestructor.sounds;
 
 public interface VolumeControl {
-    void setVolume(int volume);
-    int getVolume();
+    void setVolume(float volume);
+    float getVolume();
 }
