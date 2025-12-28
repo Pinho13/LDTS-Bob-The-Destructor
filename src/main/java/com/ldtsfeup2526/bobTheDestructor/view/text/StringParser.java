1b658737914a3dfc33fe10d4cc6b245142c340c6
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-28 17:16:38 +0000

    Added Space Sprite

--- src/main/java/com/ldtsfeup2526/bobTheDestructor/view/text/StringParser.java
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/view/text/StringParser.java
@@ -7,5 +7,6 @@ import java.io.IOException;
 public class StringParser extends FontParser {
     public StringParser(SpriteLoader spriteLoader, String font, int distanceBetweenChars) throws IOException {
         super(spriteLoader, font, "_-:!?.()[]|+=/0123456789abcdefghijklmnopqrstuvwxyz", distanceBetweenChars);
+        add(' ', "sprites/ui/letters/space.png");
     }
 }
