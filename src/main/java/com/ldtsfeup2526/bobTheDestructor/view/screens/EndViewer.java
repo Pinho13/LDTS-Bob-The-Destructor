9d40edffab0fc6eea715749bdd023a821a3278f7
Pinho <rafaelpinhotsilva@gmail.com>
2025-12-29 16:42:52 +0000

    Added EndState and EndViewer

---
+++ src/main/java/com/ldtsfeup2526/bobTheDestructor/view/screens/EndViewer.java
@@ -0,0 +1,33 @@
+package com.ldtsfeup2526.bobTheDestructor.view.screens;
+
+import com.googlecode.lanterna.TextColor;
+import com.ldtsfeup2526.bobTheDestructor.Game;
+import com.ldtsfeup2526.bobTheDestructor.gui.GUI;
+import com.ldtsfeup2526.bobTheDestructor.model.credits.Credits;
+import com.ldtsfeup2526.bobTheDestructor.model.spatials.Position;
+import com.ldtsfeup2526.bobTheDestructor.model.stats.Stats;
+import com.ldtsfeup2526.bobTheDestructor.view.ViewerProvider;
+import com.ldtsfeup2526.bobTheDestructor.view.menu.TitleViewer;
+import com.ldtsfeup2526.bobTheDestructor.view.menu.WallpaperViewer;
+
+import java.io.IOException;
+
+public class EndViewer extends ScreenViewer<Stats> {
+    private final WallpaperViewer wallpaperViewer;
+    private final TitleViewer titleViewer;
+
+    public EndViewer(Stats model, ViewerProvider viewerProvider) {
+        super(model);
+        wallpaperViewer = viewerProvider.getWallpaperViewer();
+        titleViewer = viewerProvider.getTitleViewer();
+    }
+
+    @Override
+    public void draw(GUI gui, double deltaTime) throws IOException {
+        gui.drawBackground(new TextColor.RGB(57, 53, 74));
+
+        wallpaperViewer.draw(gui);
+
+        titleViewer.draw(new Position(Game.resolution.width()/2 + 1, 10), "Thank you for playing!", gui);
+    }
+}
