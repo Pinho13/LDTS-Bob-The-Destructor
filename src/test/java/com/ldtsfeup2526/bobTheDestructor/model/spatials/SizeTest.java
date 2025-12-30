d3481bbedad0a3d11121ba06f3cea1d450c01017
T0m2sT <pedrotomasteixeira@gmail.com>
2025-12-30 03:04:42 +0000

    Added `testConstructorWithNumberTypes` in `SizeTest` to validate type casting logic for `Size` constructor.

--- src/test/java/com/ldtsfeup2526/bobTheDestructor/model/spatials/SizeTest.java
+++ src/test/java/com/ldtsfeup2526/bobTheDestructor/model/spatials/SizeTest.java
@@ -12,6 +12,12 @@ public class SizeTest {
     }
 
     @Test
+    void testConstructorWithNumberTypes() {
+        Size s1 = new Size(10.5, 20.7);
+        assertEquals(10, s1.getX());
+        assertEquals(20, s1.getY());
+    }
+    @Test
     void testCopyConstructor() {
         Size size1 = new Size(100, 200);
         Size size2 = new Size(size1);
