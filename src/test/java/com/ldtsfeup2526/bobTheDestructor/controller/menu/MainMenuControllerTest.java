10b3fe4d485c8b8ccd2e17518ea58d6c0a67beda
T0m2sT <pedrotomasteixeira@gmail.com>
2025-12-30 03:00:44 +0000

    Refactored `MainMenuControllerTest` by removing sound-related tests, introducing `WidgetController` dependency, and improving action-handling test coverage.

--- src/test/java/com/ldtsfeup2526/bobTheDestructor/controller/menu/MainMenuControllerTest.java
+++ src/test/java/com/ldtsfeup2526/bobTheDestructor/controller/menu/MainMenuControllerTest.java
@@ -3,99 +3,58 @@ package com.ldtsfeup2526.bobTheDestructor.controller.menu;
 import com.ldtsfeup2526.bobTheDestructor.Game;
 import com.ldtsfeup2526.bobTheDestructor.controller.input.Action;
 import com.ldtsfeup2526.bobTheDestructor.model.menu.MainMenu;
-import javax.sound.sampled.Clip;
-import javax.sound.sampled.FloatControl;
-import com.ldtsfeup2526.bobTheDestructor.sounds.SoundPlayer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import java.io.IOException;
 import java.util.List;
+
 import static org.mockito.Mockito.*;
 
 public class MainMenuControllerTest {
     private MainMenu menu;
     private MainMenuController controller;
     private Game game;
-    private SoundPlayer soundPlayer;
+    private WidgetController widgetController;
 
     @BeforeEach
-    void setUp() throws IOException {
+    void setUp() {
         menu = mock(MainMenu.class);
-        soundPlayer = mock(SoundPlayer.class);
-        when(menu.getSoundPlayer()).thenReturn(soundPlayer);
-
-        when(soundPlayer.getSound()).thenReturn(null);
-        
-        controller = new MainMenuController(menu);
+        widgetController = mock(WidgetController.class);
+        controller = new MainMenuController(menu, widgetController);
         game = mock(Game.class);
     }
 
     @Test
-    void testOnQuit() {
-        controller.onQuit(game);
-        verify(game).setState(null);
-    }
-
-    @Test
     void testUpdateUp() throws IOException {
-        controller.update(game, List.of(Action.UP));
+        controller.update(game, List.of(Action.UP), 0.1);
         verify(menu).moveUp();
+        verify(widgetController).updateWidgetState();
     }
 
     @Test
     void testUpdateDown() throws IOException {
-        controller.update(game, List.of(Action.DOWN));
+        controller.update(game, List.of(Action.DOWN), 0.1);
         verify(menu).moveDown();
+        verify(widgetController).updateWidgetState();
     }
 
     @Test
     void testUpdateQuit() throws IOException {
-        controller.update(game, List.of(Action.QUIT));
+        controller.update(game, List.of(Action.QUIT), 0.1);
         verify(game).setState(null);
-    }
-
-    @Test
-    void testConstructorWithSound() {
-        MainMenu menuWithSound = mock(MainMenu.class);
-        SoundPlayer player = mock(SoundPlayer.class);
-        Clip clip = mock(Clip.class);
-        FloatControl control = mock(FloatControl.class);
-        
-        when(menuWithSound.getSoundPlayer()).thenReturn(player);
-        when(player.getSound()).thenReturn(clip);
-        when(clip.isControlSupported(FloatControl.Type.MASTER_GAIN)).thenReturn(true);
-        when(clip.getControl(FloatControl.Type.MASTER_GAIN)).thenReturn(control);
-
-        com.ldtsfeup2526.bobTheDestructor.model.GameSettings.getInstance().setMasterGain(5.0f);
-
-        new MainMenuController(menuWithSound);
-
-        verify(control).setValue(-15.0f);
-        verify(player).start();
-    }
-
-    @Test
-    void testConstructorWithSoundNoControl() {
-        MainMenu menuWithSound = mock(MainMenu.class);
-        SoundPlayer player = mock(SoundPlayer.class);
-        Clip clip = mock(Clip.class);
-        
-        when(menuWithSound.getSoundPlayer()).thenReturn(player);
-        when(player.getSound()).thenReturn(clip);
-        when(clip.isControlSupported(FloatControl.Type.MASTER_GAIN)).thenReturn(false);
-        
-        new MainMenuController(menuWithSound);
-        
-        verify(player).start();
     }
 
     @Test
     void testUpdateOtherAction() throws IOException {
-        com.ldtsfeup2526.bobTheDestructor.model.menu.Button button = mock(com.ldtsfeup2526.bobTheDestructor.model.menu.Button.class);
-        when(menu.getCurrentButton()).thenReturn(button);
-        when(button.getButtonType()).thenReturn(com.ldtsfeup2526.bobTheDestructor.model.menu.ButtonType.EXIT);
-        
-        controller.update(game, List.of(Action.SELECT));
-        verify(game).setState(null);
+        controller.update(game, List.of(Action.SELECT), 0.1);
+        verify(widgetController).update(eq(game), anyList(), eq(0.1));
+    }
+    @Test
+    void testUpdateMultipleActions() throws IOException {
+        controller.update(game, List.of(Action.UP, Action.DOWN), 0.1);
+        verify(menu).moveUp();
+        verify(menu).moveDown();
+        verify(widgetController, times(2)).updateWidgetState();
     }
 }
